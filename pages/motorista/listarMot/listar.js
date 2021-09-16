329cfa62dd4b01fabe8e2ca4632f5890f05d1c7c
Harry Potter <cabrerarobles@gmail.com>
2021-09-16 10:14:53 -0400

    fix: import path of layout

--- pages/motorista/listarMot/listar.js
+++ pages/motorista/listarMot/listar.js
@@ -1,13 +1,12 @@
 import React from "react";
 import CardMotorista from "../../../components/cardMotorista";
-import Layout from "../../../../components/Layout";
+import Layout from "../../../components/Layout";
 
 const Listar = () => {
   return (
     <Layout>
       <CardMotorista />
     </Layout>
-
-  )
+  );
 };
 export default Listar;
